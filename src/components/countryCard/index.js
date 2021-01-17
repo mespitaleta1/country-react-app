2c2e7cb0153a0cb12f7b91df2f249abdda5fc1e1
Melissa Espitaleta <m.espitaleta1@gmail.com>
2021-01-17 11:51:28 -0500

    final list version

--- src/components/countryCard/index.js
+++ src/components/countryCard/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Coin from "../icons/Coin";
 import Lang from "../icons/Lang";
@@ -11,38 +12,38 @@ import styles from "./styles.module.css";
 const CountryCard = (props) => {
   return (
     <Card>
-      <div className={styles.cardWrapper}>
-        <div>
-          <img className={styles.flag} src={props.flag} alt={props.name} />
+      <Card.Body>
+        <div className="d-flex justify-content-start">
+          <div>
+            <img className={styles.flag} src={props.flag} alt={props.name} />
+          </div>
+
+          <div>
+            <Card.Title className={styles.name}>{props.name}</Card.Title>
+            <Card.Subtitle>{props.capital}</Card.Subtitle>
+            <Card.Text className={styles.region}>{props.region}</Card.Text>
+          </div>
         </div>
-        <div>
-          <Card.Body>
-            <Row>
-              <Col>
-                <Card.Title>{props.name}</Card.Title>
-                <Card.Subtitle>{props.capital}</Card.Subtitle>
-              </Col>
-              <Col>
-                <Card.Text>{props.region}</Card.Text>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Lang />
-                {props.languages.map((language, index) => (
-                  <span key={index}>{language["iso639_2"]} -</span>
-                ))}
-              </Col>
-              <Col>
-                <Coin />
-                {props.currencies.map((currency, index) => (
-                  <span key={index}>{currency.code} -</span>
-                ))}
-              </Col>
-            </Row>
-          </Card.Body>
+        <div className={styles.detail}>
+          <div className="d-flex justify-content-start align-items-center">
+            <Lang className={styles.icon} />
+            <div>
+              {props.languages.map((language, index) => (
+                <span key={index}>{language["iso639_2"]} /</span>
+              ))}
+            </div>
+          </div>
+
+          <div className="d-flex justify-content-start align-items-center">
+            <Coin className={styles.icon} />
+            <div>
+              {props.currencies.map((currency, index) => (
+                <span key={index}>{currency.code} /</span>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
+      </Card.Body>
     </Card>
   );
 };
