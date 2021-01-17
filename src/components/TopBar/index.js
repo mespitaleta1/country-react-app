ab5d6a62f1b02acc8501301cf9398af44d68ad50
Melissa Espitaleta <m.espitaleta1@gmail.com>
2021-01-17 10:01:45 -0500

    adding the searchBar style

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { InputGroup } from "react-bootstrap";
+import { FormControl } from "react-bootstrap";
+import Lens from "../icons/Lens";
+
+//import Input from "react-bootstrap/Input";
 
 const SearchBar = () => {
-  return <p>Search Bar</p>;
+  return (
+    <InputGroup>
+      <InputGroup.Prepend>
+        <InputGroup.Text id="btnSearchBar">
+          <Lens />
+        </InputGroup.Text>
+      </InputGroup.Prepend>
+      <FormControl
+        type="text"
+        placeholder="Search your favorite country"
+        aria-label="Search"
+        aria-describedby="btnSearchBar"
+      />
+    </InputGroup>
+  ); //<input type="text" placeholder="Search.."></input>;
 };
 
 const FilterBar = () => {
--- src/components/countryCard/index.js
+++ src/components/countryCard/index.js
@@ -3,44 +3,46 @@ import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Coin from "../icons/Coin";
+import Lang from "../icons/Lang";
+
+import styles from "./styles.module.css";
 
 const CountryCard = (props) => {
   return (
     <Card>
-      <Container>
-        <Row>
-          <Col xs={2}>
-            <img src={props.flag} alt={props.name} width="200" />
-          </Col>
-          <Col xs={10}>
-            <Card.Body>
-              <Container>
-                <Row>
-                  <Col>
-                    <Card.Title>{props.name}</Card.Title>
-                    <Card.Subtitle>{props.capital}</Card.Subtitle>
-                  </Col>
-                  <Col>
-                    <Card.Text>{props.region}</Card.Text>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>
-                    {props.languages.map((language, index) => (
-                      <span key={index}>{language["iso639_2"]} -</span>
-                    ))}
-                  </Col>
-                  <Col>
-                    {props.currencies.map((currency, index) => (
-                      <span key={index}>{currency.code} -</span>
-                    ))}
-                  </Col>
-                </Row>
-              </Container>
-            </Card.Body>
-          </Col>
-        </Row>
-      </Container>
+      <div className={styles.cardWrapper}>
+        <div>
+          <img className={styles.flag} src={props.flag} alt={props.name} />
+        </div>
+        <div>
+          <Card.Body>
+            <Row>
+              <Col>
+                <Card.Title>{props.name}</Card.Title>
+                <Card.Subtitle>{props.capital}</Card.Subtitle>
+              </Col>
+              <Col>
+                <Card.Text>{props.region}</Card.Text>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Lang />
+                {props.languages.map((language, index) => (
+                  <span key={index}>{language["iso639_2"]} -</span>
+                ))}
+              </Col>
+              <Col>
+                <Coin />
+                {props.currencies.map((currency, index) => (
+                  <span key={index}>{currency.code} -</span>
+                ))}
+              </Col>
+            </Row>
+          </Card.Body>
+        </div>
+      </div>
     </Card>
   );
 };
