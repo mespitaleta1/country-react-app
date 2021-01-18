ee1a4f85168f6604d2761b69f698ef08875b559b
Melissa Espitaleta <m.espitaleta1@gmail.com>
2021-01-18 08:34:50 -0500

    final details

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Container from "react-bootstrap/Container";
 
-import "./App.css";
 import CountryList from "../countryList";
 import FilterBar from "../filterBar";
 import Hero from "../hero";
@@ -11,27 +10,26 @@ const App = () => {
   const [countries, setCountries] = useState([]);
   const [currentFilter, setCurrentFilter] = useState();
 
-  const getUrl = (filter) => {
-    const baseUrl = "https://restcountries.eu/rest/v2/";
-    const fieldsUrl =
-      "fields=flag;name;capital;region;currencies;languages;altSpellings;alpha3Code;topLevelDomain;population;subregion;latlng";
-    if (currentFilter) {
-      return `${baseUrl}${filter.active}/${filter.value}?${fieldsUrl}`;
-    } else {
-      return `${baseUrl}all?${fieldsUrl}`;
-    }
-  };
-
   useEffect(() => {
+    const getUrl = () => {
+      const baseUrl = "https://restcountries.eu/rest/v2/";
+      const fieldsUrl =
+        "fields=flag;name;capital;region;currencies;languages;altSpellings;alpha3Code;topLevelDomain;population;subregion;latlng";
+      if (currentFilter) {
+        return `${baseUrl}${currentFilter.active}/${currentFilter.value}?${fieldsUrl}`;
+      } else {
+        return `${baseUrl}all?${fieldsUrl}`;
+      }
+    };
     axios
-      .get(getUrl(currentFilter))
+      .get(getUrl())
       .then((res) => {
         setCountries(res.data);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, [currentFilter, getUrl]);
+  }, [currentFilter]);
 
   const handleFilterSubmit = (filter) => {
     setCurrentFilter(filter);
